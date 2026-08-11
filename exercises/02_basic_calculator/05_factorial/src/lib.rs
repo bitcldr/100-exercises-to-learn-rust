b61ba4578df37f9aad1abb1b60598617545df6c1
Dmitry Fisenko <fisenkodv@gmail.com>
2026-08-10 23:14:23 -0400

    feat: complete extercises

--- exercises/02_basic_calculator/05_factorial/src/lib.rs
+++ exercises/02_basic_calculator/05_factorial/src/lib.rs
@@ -10,6 +10,14 @@
 //
 // Use only what you learned! No loops yet, so you'll have to use recursion!
 
+fn factorial(i: i32) -> i32 {
+    if i == 0 {
+        return 1;
+    }
+
+    i * factorial(i - 1)
+}
+
 #[cfg(test)]
 mod tests {
     use crate::factorial;
